fd470b8d94b899e727175c2c45b54292bc580e48
JoaoGuilhermmy <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>
2025-10-08 10:33:05 -0300

    funções adicionadas

--- bst.h
+++
@@ -1,7 +0,0 @@
-#ifndef BST_H
-#define BST_H
-#include "models.h"
-
-CategoryNode *new_node(int id, char category_name[], char books_file[]) {};
-CategoryNode *insert_category_node(CategoryNode *root, int id, char category_name[], char books_file[]) {};
-#endif
